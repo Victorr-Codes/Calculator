c8bcccce3c7864f2637cb4728817f91ee569e83f
Alison-Code <72189697+Alison-Code@users.noreply.github.com>
2020-10-01 21:07:51 +0530

    Update CALCULATOR.py

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -1,16 +1,25 @@
-whil (1):
-    print("\nWELCOME TO SVANG's CALCULATOR")
-    print("ENTER YOUR FIRNT TO DO")
-    print("+" ,"-" ,"/" , "*")
-    var3 = input()
-    print("ENTER YOUR))
+try:
+    while (1):
+        print("\nWELCOME TO SHIVANG's CALCULATOR")
+        print("ENTER YOUR FIRST NUMBER")
+        var1 = int(input())
+        print("WHAT DO YOU WANT TO DO")
+        print("+" ,"-" ,"/" , "*")
+        var3 = input()
+        print("ENTER YOUR SECOND NUMBER")
+        var2 = int(input())
 
-    if var3=="+":
-        print("Your ans is" , var1 + var2)
+        if var3=="+":
+            print("Your ans is" , var1 + var2)
 
-    elif var3=="-":
-        print("Your ans is" , var1-var2)
- is" , var / var2)
+        elif var3=="-":
+            print("Your ans is" , var1-var2)
 
-    el
-        print("Your ans is" ,var1 * va
+        elif var3=="/":
+            print("Your ans is" , var1 / var2)
+
+        else:
+            print("Your ans is" ,var1 * var2)
+
+except Exception as e:
+    print("Please write correctly")
