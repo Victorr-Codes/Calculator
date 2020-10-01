ac298b4fa80e7396c728d4d9c18bb1642d515f33
Victorr-Codes <72153622+Victorr-Codes@users.noreply.github.com>
2020-10-01 20:54:06 +0530

    Update CALCULATOR.py

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -1,21 +1,16 @@
-while (1):
-    print("\nWELCOME TO SHIVANG's CALCULATOR")
-    print("ENTER YOUR FIRST NUMBER")
-    var1 = int(input())
-    print("WHAT DO YOU WANT TO DO")
+whil (1):
+    print("\nWELCOME TO SVANG's CALCULATOR")
+    print("ENTER YOUR FIRNT TO DO")
     print("+" ,"-" ,"/" , "*")
     var3 = input()
-    print("ENTER YOUR SECOND NUMBER")
-    var2 = int(input())
+    print("ENTER YOUR))
 
     if var3=="+":
         print("Your ans is" , var1 + var2)
 
     elif var3=="-":
         print("Your ans is" , var1-var2)
+ is" , var / var2)
 
-    elif var3=="/":
-        print("Your ans is" , var1 / var2)
-
-    else:
-        print("Your ans is" ,var1 * var2)
+    el
+        print("Your ans is" ,var1 * va
