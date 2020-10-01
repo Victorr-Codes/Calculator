f0ebeebe2230d0a506f59b9144ab9f31243127e5
Victorr-Codes <72153622+Victorr-Codes@users.noreply.github.com>
2020-10-01 20:33:53 +0530

    Update CALCULATOR.py

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -1,27 +1,21 @@
-try:
-    while (1):
-        print("\nWELCOME TO SHIVANG's CALCULATOR")
-        print("ENTER YOUR FIRST NUMBER")
-        var1 = int(input())
-        print("WHAT DO YOU WANT TO DO")
-        print("+" ,"-" ,"/" , "*")
-        var3 = input()
-        print("ENTER YOUR SECOND NUMBER")
-        var2 = int(input())
+while (1):
+    print("\nWELCOME TO SHIVANG's CALCULATOR")
+    print("ENTER YOUR FIRST NUMBER")
+    var1 = int(input())
+    print("WHAT DO YOU WANT TO DO")
+    print("+" ,"-" ,"/" , "*")
+    var3 = input()
+    print("ENTER YOUR SECOND NUMBER")
+    var2 = int(input())
 
-        if var3=="+":
-            print("Your ans is" , var1 + var2)
+    if var3=="+":
+        print("Your ans is" , var1 + var2)
 
-        elif var3=="-":
-            print("Your ans is" , var1-var2)
+    elif var3=="-":
+        print("Your ans is" , var1-var2)
 
-        elif var3=="/":
-            print("Your ans is" , var1 / var2)
+    elif var3=="/":
+        print("Your ans is" , var1 / var2)
 
-        else:
-            print("Your ans is" ,var1 * var2)
-
-except Exception as e:
-    print("Please write correctly")
-        
-    
+    else:
+        print("Your ans is" ,var1 * var2)
